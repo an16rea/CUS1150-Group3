a74c53e794f3542e9ed505e39dee8f5bdd239c10
Andrea Dass <andrea.dass16@gmail.com>
2026-04-27 23:39:25 -0400

    Refactor add_task.h for improved readability
    Updated comments for clarity and added newline at end of file.

--- functions/add_task.h
+++ functions/add_task.h
@@ -1,37 +1,30 @@
 #include <stdbool.h>
 
+// Represents the urgency level of a task
 typedef enum {
-
     LOW,
     MEDIUM,
     HIGH
-
 } Priority;
 
+// Represents which area of category the task belongs to
 typedef enum {
-
     SCHOOL,
     WORK,
     LIFE,
     GENERAL
-
 } Categories;
 
-
-
+// Represents a single task in the todo list
 typedef struct {
-    int id;
-    char name[30];
-    bool completed;
-    char timestamp[20];
-    char deadline[20];
-    Priority priority; 
-    Categories categories;
-
+    int id;        // Unique identifier for the task
+    char name[30];        // Name/Description of the task
+    bool completed;        // true if task is done, false if not
+    char timestamp[20];        // date the task was created (MM-DD-YYYY)
+    char deadline[20];        // date the task is due (MM-DD-YYYY)
+    Priority priority;         // Urgency level: Low, MEDIUM, OR HIGH
+    Categories categories;        // Which category the task belongs to
 } TodoItem; 
 
-
-
+// Appends a task to database.txt
 int add_task(TodoItem *task);
-// int read_tasks();
-// int update_task();
